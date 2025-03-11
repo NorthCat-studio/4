5625f170854b030dd379577c40853e4daa53e6d0
MIC-ael <blackcatkyr@gmail.com>
2025-03-11 16:45:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+   
+
+  
+
+    Object.keys(navLinks).forEach(key => {
+        navLinks[key].addEventListener('click', (e) => {
+            e.preventDefault();
+            showSection(key);
+        });
+    });
+
+    function showSection(sectionId) {
+        Object.keys(sections).forEach(key => {
+            if (sections[key]) {
+                sections[key].style.display = 'none';
+            }
+        });
+        sections[sectionId].style.display = 'block';
+    }
+
+    showSection('home'); // Initially display the home section
+
+    
+
+   
+    
+        });
+    }
+});
